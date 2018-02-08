a03f814ccf3b1f5770b58ff7659a4ffa45a2d945
Stephen Rogers <stephenrogers74@yahoo.com>
2018-02-08 17:23:25 -0600

    Getting Close

--- public_html/script.js
+++ public_html/script.js
@@ -7,14 +7,44 @@ function printUserInput()
 {
 var firstName = document.getElementById('firstName').value;
 var lastName = document.getElementById('lastName').value;
-var age = document.getElementById('age').value;
-var phoneNumber=document.getElementById('phoneNumber').value;
+var age = parseInt(document.getElementById('age').value);
+var phoneNumber=parseInt(document.getElementById('phoneNumber').value);
 var address=document.getElementById('address').value;
 console.log(firstName);
 console.log(lastName);
 console.log(age);
 console.log(phoneNumber);
 console.log(address);
+document.getElementById("postFullName").innerHTML =firstName+" "+lastName;
+
+
+    var numberArray=[];
+    numberArray.push(age,phoneNumber);
+    console.log(numberArray);
+
+   for(var i=0;i<numberArray.length;i++)
+    {
+        if(numberArray[i]<100)
+        {
+            //age
+            document.getElementById("postAge").innerHTML =numberArray[i];
+
+        }
+        else if(numberArray[i]>100)
+        {
+            //phone number
+            
+            document.getElementById("postPhoneNumber").innerHTML=numberArray[i];
+        }
+    }
+
+
+document.getElementById("postAddress").innerHTML = address;
+
+
+
+
+
 }
 btn.addEventListener('click',printUserInput);
     
@@ -36,17 +66,7 @@ var firstName;
     var numberArray=[];
     numberArray.push(age,phoneNumber);
     
-    for(var i=0;i<numberArray.lenth;i++)
-    {
-        if(numberArray[i]<100)
-        {
-            //age
-        }
-        else if(numberArray[i]>100)
-        {
-            //phone number
-        }
-    }
+ 
     
     
     

d8e7e68a6106cb00364b97306534dba2e7b5ff37
Stephen Rogers <stephenrogers74@yahoo.com>
2018-02-08 15:32:13 -0600

    Ready to start on ID Card

--- public_html/script.js
+++ public_html/script.js
@@ -1,8 +1,29 @@
 
 
+
+    var btn = document.getElementById("btn");
+
+function printUserInput()
+{
+var firstName = document.getElementById('firstName').value;
+var lastName = document.getElementById('lastName').value;
+var age = document.getElementById('age').value;
+var phoneNumber=document.getElementById('phoneNumber').value;
+var address=document.getElementById('address').value;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(phoneNumber);
+console.log(address);
+}
+btn.addEventListener('click',printUserInput);
+    
+    
+
 function idCard()
 {
-    var firstName;
+
+var firstName;
     var lastName;
     var address;
     

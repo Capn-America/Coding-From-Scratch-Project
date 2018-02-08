997a53a5672eb00168c0d3c63d3b0d3d8391cade
Stephen Rogers <stephenrogers74@yahoo.com>
2018-02-08 10:26:04 -0600

    Update 1: Adding general info

--- public_html/script.js
+++ public_html/script.js
@@ -1,7 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+
+function idCard()
+{
+    var firstName;
+    var lastName;
+    var address;
+    var age;
+    var phoneNumber;
+    var numberArray=[];
+    
+    
+    
+}
 

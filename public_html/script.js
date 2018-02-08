f068ccebafa56286b29787d0147b1cdfb680a530
Stephen Rogers <stephenrogers74@yahoo.com>
2018-02-08 10:51:46 -0600

    Update to java file

--- public_html/script.js
+++ public_html/script.js
@@ -5,9 +5,28 @@ function idCard()
     var firstName;
     var lastName;
     var address;
+    
+    
+    
+    
+    
     var age;
     var phoneNumber;
     var numberArray=[];
+    numberArray.push(age,phoneNumber);
+    
+    for(var i=0;i<numberArray.lenth;i++)
+    {
+        if(numberArray[i]<100)
+        {
+            //age
+        }
+        else if(numberArray[i]>100)
+        {
+            //phone number
+        }
+    }
+    
     
     
     
